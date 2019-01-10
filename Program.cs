75e03dc908260987206f867f4082df741464740a
Brian Liu <CHBrian.Liu@outlook.com>
2019-01-10 20:57:28 +0800

    Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LookupTheVoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
