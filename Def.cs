ec51997a2919f140f475bfcd82b5adc6f81b83f8
Brian Liu <CHBrian.Liu@outlook.com>
2019-03-20 20:47:16 +0800

    updates .gitignore. define searching result.

---
+++ Def.cs
@@ -0,0 +1,9 @@
+namespace LookupTheVoc
+{
+    public class Definition
+    {
+        public string id {get; set;}
+        public string PartOfSpeech {get; set;}
+        public string[] def {get; set;}
+    }
+}
