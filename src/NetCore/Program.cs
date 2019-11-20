68ea03919d385eceb46fe1e8a180822eb5c246fa
Brian Liu <CHBrian.Liu@outlook.com>
2019-11-20 20:30:15 +0800

    Add .Net Core Project in src/NetCore/

---
+++ src/NetCore/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Net.Sockets;
+
+namespace LookupVoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
