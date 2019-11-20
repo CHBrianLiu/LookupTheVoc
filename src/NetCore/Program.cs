c91b8acb9ff8bdde443b6f8a4b07c3606be8894b
Brian Liu <CHBrian.Liu@outlook.com>
2019-11-20 23:44:38 +0800

    New dotnet core project: [To be continued] to look up the vocabulary using restful API and process the result.

--- src/NetCore/Program.cs
+++ src/NetCore/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
-using System.Net.Sockets;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace LookupVoc
 {
@@ -9,7 +10,71 @@ namespace LookupVoc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // try find key locally in credential.local file.
+            if (!File.Exists("./credential.local")) {
+                // not found. read from user input and store in credential.local file.
+                Console.WriteLine("Cannot find a valid key. Please enter your key: ");
+                string userKeyInput = Console.ReadLine();
+                File.WriteAllText("./credential.local", userKeyInput);
+            }
+
+            // read key from credential.local file and create an Profile object to store key.
+            LookupVoc.Profile profileObj = new LookupVoc.Profile(File.ReadAllText("./credential.local"));
+            if (!profileObj.isCorrectKey()) {
+                Console.WriteLine("Incorrect key detected. Please modify the key in file 'credential.local'");
+                Environment.Exit(0);
+            }
+
+            // specify read file
+            List<string> inputFilesList = new List<string>();
+            if (args.Length == 0) {
+                // read file not specified in arguments
+                Console.WriteLine("Enter the file name that contains vocabularies: ");
+                inputFilesList.Add(Console.ReadLine());
+            } else {
+                foreach (string filename in args) {
+                    inputFilesList.Add(filename);
+                }
+            }
+
+            // process each file in the list
+            foreach (string file in inputFilesList) {
+                // check if file exists
+                if (!File.Exists(file)) {
+                    Console.WriteLine(file + " not found.");
+                    continue;
+                }
+                // read all text from the file and store in a list
+                var vocs = File.ReadAllLines(file);
+                // loop the list
+                foreach (string voc in vocs) {
+                    LookupVoc.Definition def = new LookupVoc.Definition();
+                    // look up the vocabulary and process the result
+
+                    // process the output format and save in file
+                }
+            }
+
         }
+    
+        // static public bool lookup (string key, string keyword) {
+        //     string url = "https://www.dictionaryapi.com/api/v3/references/learners/json/" + keyword + "?key=" + key;
+        //     HttpClient client = new HttpClient();
+        //     Task<string> response;
+        //     try {
+        //         response = client.GetStringAsync(url);
+        //         response.Wait();
+        //     } catch {
+        //         return false;
+        //     }
+        //     parse(response.Result);
+        //     return true;
+        // }
+        // static private void parse(string jsonStr) {
+        //     var result = JsonConvert.DeserializeObject<Definition>(jsonStr);
+        //     this.id = result.id;
+        //     this.functionalLabel = result.functionalLabel;
+        //     this.shortDef = result.shortDef;
+        // }
     }
 }
